de444a0005a512eee41f3d4e70c54d581b6acaa5
NicolasDEVoB <nik85578@gmail.com>
2026-07-29 20:34:29 -0300

    Add FastAPI dependencies and initialize database models. Phase 1 completed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import models, schemas, crud
+from app.database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
